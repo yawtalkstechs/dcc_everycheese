41b318acf8a6f6f59f9ec7f79d21213eec477b60
yawtalkstechs <yawtalkstechs@gmail.com>
2022-06-14 11:14:16 +0000

    Cheese Detail page successfully added

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -5,4 +5,5 @@ app_name = "cheeses"
 
 urlpatterns = [
     path(route='', view=CheeseListView.as_view(), name='list'),
+    path(route='<slug:slug>/', view=CheeseDetailView.as_view(), name='detail'),
 ]
